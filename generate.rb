5dde6dc7e518c3fae814e48eff9d5167d53606f6
Natasha Dalal <natashadalal@gmail.com>
2022-12-11 14:10:26 -0500

    generate day code

---
+++ generate.rb
@@ -0,0 +1,20 @@
+# run as `ruby generate.rb <daynum>` - example: `ruby generate.rb 11` for day 11
+
+day_num = ARGV[0]
+template_file = File.open("./src/template.rb", 'r')
+template_file = template_file.readlines.map(&:chomp)
+line_1 = template_file[0].split(' ')
+line_1[-1] = day_num
+template_file[0] = line_1.join(' ')
+
+File.open('./src/12.rb', 'w') do |f|
+    template_file.each do |temp|
+        f.write(temp)
+        f.write("\n")
+    end
+end
+
+f = File.new("./data/#{day_num}.txt", 'w')
+f.close
+f = File.new("./data/test_#{day_num}.txt", 'w')
+f.close
---
+++ src/12.rb
@@ -0,0 +1,7 @@
+@day_number = 12
+require_relative '../helpers.rb'
+
+def run(data)
+end
+
+pp run(@data)
